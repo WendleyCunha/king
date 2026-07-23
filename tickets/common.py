e74703fb94fef14119839766a431a565931e4a88
Wendley Cunha <wendleycunha@gmail.com>
2026-07-23 16:55:47 -0300

    Fix path adjustment in common.py

--- tickets/common.py
+++ tickets/common.py
@@ -18,7 +18,7 @@ import uuid
 import html as _htmlmod
 from datetime import datetime, timezone, timedelta
 
-_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if _root not in sys.path:
     sys.path.insert(0, _root)
 
