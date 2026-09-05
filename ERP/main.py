570abebad9db593d34fc562278a46466d8e20e6a
Wendley Cunha <wendleycunha@gmail.com>
2026-09-05 16:44:00 -0300

    Create main.py

---
+++ ERP/main.py
@@ -0,0 +1,71 @@
+ABA CADASTRO.
+Cadastro de cliente completo.
+ 
+ABA PRODUTO
+Cadastro de produto.  Completo.
+Cada produto gera um qr code próprio. Cada venda gera um qr code próprio pra controle daquele item. Item único. 
+
+ABA VENDA
+Opcao, Venda, buscando cliente e vinculando ao produto. 
+
+ABA PRODUTO
+Na tela de produto, ter opção de chegada de item com quantidade.  Isso deve alimentar o estoque, dar baixa do item ao vender. Isso vai gerar uma encomenda que deve  cair na ABA ENCOMENDAR para compras.
+
+ABA AGENDAR 
+Opcao de agendar entrega do pedido, somente após venda com estoque.
+
+Se a venda for feita sem estoque o pedido nasce bloqueado. 
+
+ABA PRODUTO
+opção para dar entrada na quantidade de itens que chegou.
+Assim que a peça da entrada na tela de estoque,  o sistema libera o pedido  bloqueado e some o saldo do estoque. 
+
+ABA DIRETORIA
+Aba Administrativa permite cancelar pedido/Eliminar pedido. 
+
+Se for um pedido LIBERADO.  O saldo o item retorna ao estoque.  
+Se for um pedido BLOQUEADO.  sai do planejamento de compras na Aba encomendar. 
+
+
+Criar usuarios e determinar que  funções cada um pode ter dentro de cada ABA
+
+
+Após escolher uma data de entrega na ABA AGENDAMENTO, o pedido vai para ABA LOGÍSTICA 
+
+Na ABA LOGÍSTICA é possível filtrar pedidos agendados por data. Uma vez selecionado e atribuído um Caminhão,  nao é mais possível executar nenhuma ação descrita até aqui. Ou seja. Não é possível,  eliminar o pedido, alterar data de agendamento.
+
+na ABA LOGISTICA é possível marcar vários pedidos para entrega formando uma carga.
+é possível retirar um pedido de carga.
+é possível adicionar um pedido na carga.
+
+ABA MOTORISTA mostra, a quantidade de entregas por motorista. é daqui que sai a informação do APP ENTREGAS onde cada motorista, consegue ver apenas suas entregas
+
+ABA MOTORISTA permite cadastrar motoristas, nome, placa. Esses dados são utilizados no momento da montagem da carga da ABA LOGISTICA.
+
+Uma vez que a carga está montada, finalizada, na ABA LOGISTICA tem opção de FATURAR PEDIDO. é aqui que a NOTA FISCAL é gerada.
+
+ABA EMPRESA com os dados da empresa, esses são os dados que vai aparecer na emissão da NOTA FISCAL quando o pedido for faturado na ABA LOGISTICA.
+
+ABA FINANCEIRO é onde consta os valores das vendas, tipo de pagamento, quem vendeu.
+
+ABA FISCAL vê todas as notas emitidas, com filtros por dia.
+
+ABA RH, consegue Cadastrar funcionários, suas funções, inserir todos os dados do colaborador e salários, e cargos.
+Na ABA RH é possível, por meio do cargo, determinar se o colaborador deve ter perfil de VENDEDOR. Se isso for selecionado, o sistema gera um CÓDIGO DE VENDEDOR, DIFERENTE DO CÓDIGO DO COLABORAR. é esse código de vendedor que vai servir para ele receber comissão.
+
+nessa ABA RH é possível ver em tempo real o valor de comissão a ser paga por colaborador, de acordo com o que foi cadastrado no ato da venda pela ABA VENDAS.
+
+ABA DIRETORIA é quem cadastra salários, e regras para pagamento de comissão. porcentagens sobre a venda individual ou venda da loja ou venda da rede.
+
+
+ABA DIRETORIA permite ajustar um saldo de produtos cadastrada errado. Quando for digitado entrada incorreta do item (SKU)
+
+Somente a ABA DIRETORIA Permite eliminar o pedido.
+
+Quando um pedido for retirado de carga na ABA LOGISTICA, chega uma notificação para ABA DIRETORIA e na ABA RH e ABA AGENDAR. Quando o pedido é retirado de carga o numero do pedido permanece igual, permanece com o saldo o produto e sai a data de agendamento. O Saldo do SKU não retorno para a ABA PRODUTO. Permanece no produto.
+
+
+
+
+
+
